fa64bc3c73a8c53dd32b68b5e766b65a1a1acd2d
Admin <Admin@DESKTOP-A4LVLFL>
2024-12-12 10:11:30 +0300

    Добавьте файлы проекта.

---
+++ calculmvvm2/CalculatorModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculmvvm2
+{
+    public class CalculatorModel
+    {
+            
+        public double Add(double a, double b) => a + b;
+        public double Subtract(double a, double b) => a - b;
+        public double Multiply(double a, double b) => a * b;
+        public double Divide(double a, double b) => b != 0 ? a / b : 0;
+    }
+
+}
---
+++ calculmvvm2/CalculatorViewModel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using calculmvvm2;
+
+public class CalculatorViewModel : INotifyPropertyChanged
+{
+    private readonly CalculatorModel _model;
+    private string _display;
+    private double _currentValue;
+    private string _currentOperation;
+
+    public string Display
+    {
+        get => _display;
+        set
+        {
+            _display = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public CalculatorViewModel()
+    {
+        _model = new CalculatorModel();
+        Display = "0";
+    }
+
+    public ICommand NumberCommand => new RelayCommand<string>(NumberPressed);
+    public ICommand OperationCommand => new RelayCommand<string>(OperationPressed);
+    public ICommand ClearCommand => new RelayCommand(Clear);
+    public ICommand EqualsCommand => new RelayCommand(Calculate);
+
+    private void NumberPressed(string number)
+    {
+        if (Display == "0")
+            Display = number;
+        else
+            Display += number;
+    }
+
+    private void OperationPressed(string operation)
+    {
+        _currentValue = double.Parse(Display);
+        _currentOperation = operation;
+        Display = "0";
+    }
+
+    private void Calculate()
+    {
+        double secondValue = double.Parse(Display);
+        double result = 0;
+
+        switch (_currentOperation)
+        {
+            case "Add":
+                result = _model.Add(_currentValue, secondValue);
+                break;
+            case "Subtract":
+                result = _model.Subtract(_currentValue, secondValue);
+                break;
+            case "Multiply":
+                result = _model.Multiply(_currentValue, secondValue);
+                break;
+            case "Divide":
+                if (secondValue != 0)
+                    result = _model.Divide(_currentValue, secondValue);
+                else
+                    Display = "Error"; 
+                return; 
+        }
+
+        Display = result.ToString();
+    }
+
+    private void Clear()
+    {
+        Display = "0";
+        _currentValue = 0;
+        _currentOperation = null;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
+
+
